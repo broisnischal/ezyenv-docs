5116cef6afc5be2a82ba9278a80d5f0c62dc1155
broisnischal <neeswebservice@gmail.com>
2025-02-15 18:39:46 +0545

    feat:init-docs

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,9 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ezyenv | easy env example" },
+    { name: "description", content: "ezyenv, easy env example | Create a sample env file from your existing .env file" },
   ];
 }
 
@@ -13,5 +12,66 @@ export function loader({ context }: Route.LoaderArgs) {
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return (
+
+    <div className="px-20 py-10 flex flex-col gap-2">
+      <h1>ezyenv</h1>
+
+      <p>
+        Push env to github but using ezyenv which is tool to automatically generate sample env files from your existing .env files! making example env file, easy, intutive to share. This helps teams share environment variable templates without exposing actual credentials.
+      </p>
+
+      <h2>Quick Use - Installation</h2>
+
+      <p>
+        # npm
+        npx ezyenv
+
+
+      </p>
+
+      # bun
+      bunx ezyenv
+
+      # yarn
+      yarn global add ezyenv
+
+      # pnpm
+      pnpm add -g ezyenv
+
+
+      <p><h1>
+        Quick Install CLI
+
+      </h1>
+        curl -fsSL https://l.nischal.pro/env | sudo sh
+        brew install ezyenv
+
+      </p>
+
+
+      <h1>Usage Guide</h1>
+      ezyenv # creates `.example` files for each env
+
+      ezyenv .env # specifies the file
+
+      ezyenv rm # removes the example files
+
+      ezyenv .env --sample sample # specifies the sample (sample|example|eg|default)
+
+
+      🚀 Automatically generates .env.example from .env files
+      🔒 Multiple environment variable support
+      💡 Single file support ezyenv test.env
+      ⚡️ Simple one-command operation
+      🔒 Preserves structure while removing sensitive values
+      🔒 Preserves single & Multi line comment
+      ⚡️ Remove the example files, ezyenv rm
+      🚀 add the example sample ext, ezyenv .env --sample (sample|example|eg|default)
+
+
+
+    </div>
+
+  );
 }
